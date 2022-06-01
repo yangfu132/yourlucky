a2d4bb56007fd7c92d1dafadaf9f03e69410bb2a
fl.yang01 <fl.yang01@zuche.com>
2022-06-01 10:20:34 +0800

    demo：修改文件名

--- lib/src/3L_Business/EasyStrategyResult/StrategyResult/SABEasyStrategyResultBusiness.dart
+++ lib/src/3L_Business/EasyStrategyResult/StrategyResult/SABEasyStrategyResultBusiness.dart
@@ -1,7 +1,7 @@
 ﻿import 'package:yourlucky/src/3L_Business/EasyDetail/SABEasyDetailModel.dart';
 import 'package:yourlucky/src/3L_Business/StoreEasy/SABEasyDigitModel.dart';
 
-import '../Strategy/SABEasyStrategyModel.dart';
+import '../Strategy/SABEasyStrategyInfoModel.dart';
 import 'SABEasyAvoidBusiness.dart';
 import 'SABEasyStrategyResultModel.dart';
 
@@ -14,7 +14,7 @@ class SABEasyStrategyResultBusiness {
   SABEasyStrategyResultModel resultModel() {
     SABEasyStrategyResultModel modelResult;
     switch (strategy) {
-      case SABEasyStrategyModel.avoid:
+      case SABEasyStrategyInfoModel.avoid:
         SABEasyAvoidBusiness avoidBusiness = SABEasyAvoidBusiness(inputDetail);
         modelResult = avoidBusiness.resultModel();
         break;
