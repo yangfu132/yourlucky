f2dd079d47bbdd08b1266e06cf91cffae154eaf7
fl.yang01 <fl.yang01@zuche.com>
2023-12-14 10:27:12 +0800

    feature：增加设置界面

--- lib/src/B_UI/User/Setting/SAUSettingRoute.dart
+++ lib/src/B_UI/User/Setting/SAUSettingRoute.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class SAUSettingRoute extends StatefulWidget {
   SAUSettingRoute({Key? key, this.title}) : super(key: key);
   final String? title;
+  bool switchValue = false;
   @override
   SAUSettingRouteState createState() {
     return SAUSettingRouteState();
@@ -16,14 +17,36 @@ class SAUSettingRouteState extends State<SAUSettingRoute> {
   }
 
   Widget _buildBody() {
-    return Text('Waiting');
+    // return Text('Waiting');
+    return ListView.builder(itemCount: 10,itemBuilder: (BuildContext context, int index){
+      return _rowWidget(widget.switchValue);
+    },);
+  }
+
+  Widget _rowWidget (bool switchValue){
+    return Container(
+      child:
+        Row(
+          children: [
+            Text('Waiting,WaitingWaitingWaiting'),
+            Switch(
+              value: switchValue,
+              onChanged: (value){
+                widget.switchValue = !switchValue;
+                setState(() {
+                });
+              }
+            ),
+          ],
+        ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('反馈'),
+        title: Text('设置'),
       ),
       body: _buildBody(),
     );
