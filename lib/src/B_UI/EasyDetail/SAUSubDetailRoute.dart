a691453d2fdb42fe3c65e3f3986418da7c8aaa83
fl.yang01 <fl.yang01@zuche.com>
2023-12-19 10:16:31 +0800

    feature：修改按钮标题

--- lib/src/B_UI/EasyDetail/SAUSubDetailRoute.dart
+++ lib/src/B_UI/EasyDetail/SAUSubDetailRoute.dart
@@ -10,27 +10,81 @@ class SAUSubDetailRoute extends StatefulWidget {
   SAUSubDetailRoute(this.inputDetailModel, this.intIndex);
   final SABEasyDetailModel inputDetailModel;
   final int intIndex;
-  EasyTypeEnum currentEasyType = EasyTypeEnum.from;
   late final SABDiagramsDetailModel resultModel = SABDiagramsDetailModel();
   @override
   _SAUEasyResultState createState() {
     return _SAUEasyResultState();
   }
 
-  List<Map> resultList() {
-    if (0 == intIndex) {
-      return inputDetailModel.diagramsDetailModel.resultList;
+}
+
+
+class _SAUEasyResultState extends State<SAUSubDetailRoute> {
+  EasyTypeEnum currentEasyType = EasyTypeEnum.from;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget resultActionTitle() {
+    if (0 == widget.intIndex) {
+      return Text('备注');
     } else {
-      SABRowDetailModel rowModel = inputDetailModel.rowModelAtRow(intIndex - 1);
+      return Text('切换');
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            resultTitle()),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  resultAction();
+                },
+                child: resultActionTitle(),
+                style: SACContext.textButtonStyle(),
+              ),
+            ],
+      ),
+      body: ListView.builder(
+          itemCount: resultList().length * 2,
+          //itemExtent: 50.0, //强制高度为50.0
+          itemBuilder: (BuildContext context, int index) {
+            int dataIndex = index ~/ 2;
+            int kv = index % 2;
+            Map value = resultList()[dataIndex];
+            if (kv > 0)
+              return ListTile(title: Text(value['value']));
+            else
+              return Container(
+                //color: Colors.grey,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                ),
+                child: ListTile(title: Text(value['key'])),
+              );
+            //return ListTile(title: Text(value['key']));
+          }),
+    );
+  }
+
+  List<Map> resultList() {
+    if (0 == widget.intIndex) {
+      return widget.inputDetailModel.diagramsDetailModel.resultList;
+    } else {
+      SABRowDetailModel rowModel = widget.inputDetailModel.rowModelAtRow(widget.intIndex - 1);
       return rowModel.resultList(currentEasyType);
     }
   }
 
   String resultTitle() {
-    if (0 == intIndex) {
-      return inputDetailModel.digitModel().easyRemark;
+    if (0 == widget.intIndex) {
+      return widget.inputDetailModel.digitModel().easyRemark;
     } else {
-      SABRowDetailModel rowModel = inputDetailModel.rowModelAtRow(intIndex - 1);
+      SABRowDetailModel rowModel = widget.inputDetailModel.rowModelAtRow(widget.intIndex - 1);
       String result = 'type：';
       switch(currentEasyType) {
         case EasyTypeEnum.from:
@@ -49,60 +103,13 @@ class SAUSubDetailRoute extends StatefulWidget {
     }
   }
 
-  void switchType(){
-    if (0 == intIndex) {
+  void resultAction(){
+    if (0 == widget.intIndex) {
     } else {
-      SABRowDetailModel rowModel = inputDetailModel.rowModelAtRow(intIndex - 1);
+      SABRowDetailModel rowModel = widget.inputDetailModel.rowModelAtRow(widget.intIndex - 1);
       currentEasyType = rowModel.getNextEasyType(currentEasyType);
+      setState(() {
+      });
     }
-  }
-}
-
-
-class _SAUEasyResultState extends State<SAUSubDetailRoute> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-            widget.resultTitle()),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  widget.switchType();
-                  setState(() {
-
-                  });
-                },
-                child: Text('切换'),
-                style: SACContext.textButtonStyle(),
-              ),
-            ],
-      ),
-      body: ListView.builder(
-          itemCount: widget.resultList().length * 2,
-          //itemExtent: 50.0, //强制高度为50.0
-          itemBuilder: (BuildContext context, int index) {
-            int dataIndex = index ~/ 2;
-            int kv = index % 2;
-            Map value = widget.resultList()[dataIndex];
-            if (kv > 0)
-              return ListTile(title: Text(value['value']));
-            else
-              return Container(
-                //color: Colors.grey,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                ),
-                child: ListTile(title: Text(value['key'])),
-              );
-            //return ListTile(title: Text(value['key']));
-          }),
-    );
   }
 }
