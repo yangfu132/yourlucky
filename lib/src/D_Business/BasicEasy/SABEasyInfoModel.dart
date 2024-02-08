808d7c2cf31b685816c2e0027e0d1e6d650bac2c
fl.yang01 <fl.yang01@zuche.com>
2024-02-08 15:44:15 +0800

    feature:增加列表

--- lib/src/D_Business/BasicEasy/SABEasyInfoModel.dart
+++ lib/src/D_Business/BasicEasy/SABEasyInfoModel.dart
@@ -6,53 +6,9 @@ import 'package:yourlucky/src/D_Business/BasicEasy/SABEasyInfoSymbolModel.dart';
 class SABEasyInfoModel extends SABBaseModel {
   final Map dictionary = {};
 
-  void aaa () {
-    List array = [
-      {"原文古文":"","原文译文":"","象文古文":"","象文译文":""},
-      {"原文古文":"","原文译文":"","象文古文":"","象文译文":""},
-      {"原文古文":"","原文译文":"","象文古文":"","象文译文":""},
-      {"原文古文":"","原文译文":"","象文古文":"","象文译文":""},
-      {"原文古文":"","原文译文":"","象文古文":"","象文译文":""},
-      {"原文古文":"","原文译文":"","象文古文":"","象文译文":""}];
-    SABEasyInfoSymbolModel info;
-    dictionary["010010"] = {
-      "name": "",
-      "data": array,
-      "世": (6),
-      "应": (3),
-      "info":info
-    };
-  }
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //
-//   // 第一卦：《乾卦》屯卦
+//   // 第一卦：《乾卦》
 //   //
 //   // 乾：元，亨，利，贞。
 //   //
@@ -117,6 +73,218 @@ class SABEasyInfoModel extends SABBaseModel {
 //   // 《象》曰："用九"，天德不可为首也。
 //   //
 //   // 【白话】《象辞》说："用九"的爻象说明，天虽生万物，但却不居首、不居功。
+//
+  void aaa () {
+    SABEasyInfoSymbolModel symbol1 = SABEasyInfoSymbolModel(
+        originalClassic:"初九，潜龙勿用。",
+        originalTranslate:"初九，龙尚潜伏在水中，养精蓄锐，暂时还不能发挥作用。",
+        likeClassic:"《象》曰：潜龙勿用，阳在下也。",
+        likeTranslate:"《象辞》说：龙象征阳。\"龙尚潜伏在水中，养精蓄锐，暂时还不能发挥作用\"，是因为此爻位置最低，阳气不能散发出来的缘故。");
+    SABEasyInfoSymbolModel symbol2 = SABEasyInfoSymbolModel(
+        originalClassic:"九二，见龙在田，利见大人。",
+        originalTranslate:"九二，龙已出现在地上，利于出现德高势隆的大人物。",
+        likeClassic:"《象》曰：\"见龙在田\"，德施普也。",
+        likeTranslate:"《象辞》说：\"龙已出现在地上\"，犹如阳光普照，天下人普遍得到恩惠。");
+    SABEasyInfoSymbolModel symbol3 = SABEasyInfoSymbolModel(
+        originalClassic:"九三，君子终日乾乾，夕惕若，厉无咎。",
+        originalTranslate:"九三，君子整天自强不息，晚上也不敢有丝毫的懈怠，这样即使遇到危险也会逢凶化吉。",
+        likeClassic:"《象》曰：\"终日乾乾\"，反复道也。",
+        likeTranslate:"《象辞》说：\"整天自强不息\"，是因为要避免出现反复，不敢有丝毫大意。");
+    SABEasyInfoSymbolModel symbol4 = SABEasyInfoSymbolModel(
+        originalClassic:"九四，或跃在渊，无咎。",
+        originalTranslate:"九四，龙或腾跃而起，或退居于渊，均不会有危害。",
+        likeClassic:"《象》曰：\"或跃在渊\"，进无咎也。",
+        likeTranslate:"《象辞》说：\"龙或腾跃而起，或退居于渊，均不会有危害\"，因为能审时度势，故进退自如，不会有危害。");
+    SABEasyInfoSymbolModel symbol5 = SABEasyInfoSymbolModel(
+        originalClassic:"九五，飞龙在天，利见大人。",
+        originalTranslate:"九五，龙飞上了高空，利于出现德高势隆的大人物。",
+        likeClassic:"《象》曰：\"飞龙在天\"，大人造也。",
+        likeTranslate:"《象辞》说：\"龙飞上了高空\"，象征德高势隆的大人物一定会有所作为。");
+    SABEasyInfoSymbolModel symbol6 = SABEasyInfoSymbolModel(
+        originalClassic:"上九，亢龙有悔。",
+        originalTranslate:"上九，龙飞到了过高的地方，必将会后悔。",
+        likeClassic:"《象》曰：\"亢龙有悔\"，盈不可久也。",
+        likeTranslate:"《象辞》说\"龙飞到了过高的地方，必将会后悔\"，因为物极必反，事物发展到了尽头，必将走向自己的反面。");
+    List<SABEasyInfoSymbolModel> array = [
+      symbol1,
+      symbol2,
+      symbol3,
+      symbol4,
+      symbol5,
+      symbol6];
+    SABEasyInfoSymbolModel info;
+    //dictionary["111111"] = {"name": "乾为天(乾)", "data": array, "世": 6, "应": 3};
+    dictionary["111111"] = {
+      "originalClassic":"乾：元，亨，利，贞。",
+      "originalTranslate":"《乾卦》象征天：元始，亨通，和谐，贞正。",
+      "likeClassic":"《象》曰：天行健，君子以自强不息。",
+      "likeTranslate":"《象辞》说：天道运行周而复始，永无止息，谁也不能阻挡，君子应效法天道，自立自强，不停地奋斗下去。",
+      "name": "《乾卦》",
+      "data": array,
+    };
+
+  }
+
+  Map initEasyData() {
+    Map dictionary = {};
+    dictionary["111111"] = {"name": "乾为天(乾)"};
+    dictionary["111110"] = {"name": " 天风姤(乾)"};
+    dictionary["111100"] = {"name": "天山遯(乾)"};
+    dictionary["111000"] = {"name": "天地否(乾)"};
+    dictionary["110000"] = {"name": "风地观(乾)"};
+    dictionary["100000"] = {"name": "山地剥(乾)"};
+    dictionary["101000"] = {"name": "火地晋(乾)"};
+    dictionary["101111"] = {"name": "火天大有(乾)"};
+    dictionary["010010"] = {"name": "坎为水(坎)"};
+    dictionary["010011"] = {"name": "水泽节(坎)"};
+    dictionary["010001"] = {"name": "水雷屯(坎)"};
+    dictionary["010101"] = {"name": "水火既济(坎)"};
+    dictionary["011101"] = {"name": "泽火革(坎)"};
+    dictionary["001101"] = {"name": "雷火丰(坎)"};
+    dictionary["000101"] = {"name": "地火明夷(坎)"};
+    dictionary["000010"] = {"name": "地水师(坎)"};
+    dictionary["100100"] = {"name": "艮为山(艮)"};
+    dictionary["100101"] = {"name": "山火贲(艮)"};
+    dictionary["100111"] = {"name": "山天大畜(艮)"};
+    dictionary["100011"] = {"name": "山泽损(艮)"};
+    dictionary["101011"] = {"name": "火泽睽(艮)"};
+    dictionary["111011"] = {"name": "天泽履(艮)"};
+    dictionary["110011"] = {"name": "风泽中孚(艮)"};
+    dictionary["110100"] = {"name": "风山渐(艮)"};
+    dictionary["001001"] = {"name": "震为木(震)"};
+    dictionary["001000"] = {"name": "雷地豫(震)"};
+    dictionary["001010"] = {"name": "雷水解(震)"};
+    dictionary["001110"] = {"name": "雷风恒(震)"};
+    dictionary["000110"] = {"name": "地风升(震)"};
+    dictionary["010110"] = {"name": "水风井(震)"};
+    dictionary["011110"] = {"name": "泽风大过(震)"};
+    dictionary["011001"] = {"name": "泽雷随(震)"};
+    dictionary["110110"] = {"name": "巽为木(巽)"};
+    dictionary["110111"] = {"name": "风天小畜(巽)"};
+    dictionary["110101"] = {"name": "风火家人(巽)"};
+    dictionary["110001"] = {"name": "风雷益(巽)"};
+    dictionary["111001"] = {"name": "天雷无妄(巽)"};
+    dictionary["101001"] = {"name": "火雷噬嗑(巽)"};
+    dictionary["100001"] = {"name": "山雷颐(巽)"};
+    dictionary["100110"] = {"name": "山风蛊(巽)"};
+    dictionary["101101"] = {"name": "离为火(离)"};
+    dictionary["101100"] = {"name": "火山旅(离)"};
+    dictionary["101110"] = {"name": "火风鼎(离)"};
+    dictionary["101010"] = {"name": "火水未济(离)"};
+    dictionary["100010"] = {"name": "山水蒙(离)"};
+    dictionary["110010"] = {"name": "风水涣(离)"};
+    dictionary["111010"] = {"name": "天水讼(离)"};
+    dictionary["111101"] = {"name": "天火同人(离)"};
+    dictionary["000000"] = {"name": "坤为土(坤)"};
+    dictionary["000001"] = {"name": "地雷复(坤)"};
+    dictionary["000011"] = {"name": "地泽临(坤)"};
+    dictionary["000111"] = {"name": "地天泰(坤)"};
+    dictionary["001111"] = {"name": "雷天大壮(坤)"};
+    dictionary["011111"] = {"name": "泽天夬(坤)"};
+    dictionary["010111"] = {"name": "水天需(坤)"};
+    dictionary["010000"] = {"name": "水地比(坤)"};
+    dictionary["011011"] = {"name": "兑为金(兑)"};
+    dictionary["011010"] = {"name": "泽水困(兑)"};
+    dictionary["011000"] = {"name": "泽地萃(兑)"};
+    dictionary["011100"] = {"name": "泽山咸(兑)"};
+    dictionary["010100"] = {"name": "水山蹇(兑)"};
+    dictionary["000100"] = {"name": "地山谦(兑)"};
+    dictionary["001100"] = {"name": "雷山小过(兑)"};
+    dictionary["001011"] = {"name": "雷泽归妹(兑)"};
+    return dictionary;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // 第一卦：《乾卦》屯卦
+  //
+  // 乾：元，亨，利，贞。
+  //
+  // 【白话】《乾卦》象征天：元始，亨通，和谐，贞正。
+  //
+  // 《象》曰：天行健，君子以自强不息。
+  //
+  // 【白话】《象辞》说：天道运行周而复始，永无止息，谁也不能阻挡，君子应效法天道，自立自强，不停地奋斗下去。
+  //
+  // 初九，潜龙勿用。
+  //
+  // 【白话】初九，龙尚潜伏在水中，养精蓄锐，暂时还不能发挥作用。
+  //
+  // 《象》曰：潜龙勿用，阳在下也。
+  //
+  // 【白话】《象辞》说：龙象征阳。"龙尚潜伏在水中，养精蓄锐，暂时还不能发挥作用"，是因为此爻位置最低，阳气不能散发出来的缘故。
+  //
+  // 九二，见龙在田，利见大人。
+  //
+  // 【白话】九二，龙已出现在地上，利于出现德高势隆的大人物。
+  //
+  // 《象》曰："见龙在田"，德施普也。
+  //
+  // 【白话】《象辞》说："龙已出现在地上"，犹如阳光普照，天下人普遍得到恩惠。
+  //
+  // 九三，君子终日乾乾，夕惕若，厉无咎。
+  //
+  // 【白话】九三，君子整天自强不息，晚上也不敢有丝毫的懈怠，这样即使遇到危险也会逢凶化吉。
+  //
+  // 《象》曰："终日乾乾"，反复道也。
+  //
+  // 【白话】《象辞》说："整天自强不息"，是因为要避免出现反复，不敢有丝毫大意。
+  //
+  // 九四，或跃在渊，无咎。
+  //
+  // 【白话】九四，龙或腾跃而起，或退居于渊，均不会有危害。
+  //
+  // 《象》曰："或跃在渊"，进无咎也。
+  //
+  // 【白话】《象辞》说："龙或腾跃而起，或退居于渊，均不会有危害"，因为能审时度势，故进退自如，不会有危害。
+  //
+  // 九五，飞龙在天，利见大人。
+  //
+  // 【白话】九五，龙飞上了高空，利于出现德高势隆的大人物。
+  //
+  // 《象》曰："飞龙在天"，大人造也。
+  //
+  // 【白话】《象辞》说："龙飞上了高空"，象征德高势隆的大人物一定会有所作为。
+  //
+  // 上九，亢龙有悔。
+  //
+  // 【白话】上九，龙飞到了过高的地方，必将会后悔。
+  //
+  // 《象》曰："亢龙有悔"，盈不可久也。
+  //
+  // 【白话】《象辞》说"龙飞到了过高的地方，必将会后悔"，因为物极必反，事物发展到了尽头，必将走向自己的反面。
+  //
+  // 用九，见群龙无首，吉。
+  //
+  // 【白话】用九，出现群龙也不愿意为首的现象，是很吉利的。
+  //
+  // 《象》曰："用九"，天德不可为首也。
+  //
+  // 【白话】《象辞》说："用九"的爻象说明，天虽生万物，但却不居首、不居功。
 //
 //   第二卦：《坤卦》
 //
--- lib/src/D_Business/BasicEasy/SABEasyInfoSymbolModel.dart
+++ lib/src/D_Business/BasicEasy/SABEasyInfoSymbolModel.dart
@@ -1,6 +1,15 @@
 import 'package:yourlucky/src/D_Business/Base/SABBaseModel.dart';
 
 class SABEasyInfoSymbolModel extends SABBaseModel {
+  SABEasyInfoSymbolModel({
+    required this.originalClassic,
+    required this.originalTranslate,
+    required this.likeClassic,
+    required this.likeTranslate,
+  });
   //{"原文古文":"","原文译文":"","象文古文":"","象文译文":""},
-
+  final String originalClassic; ///原文古文
+  final String originalTranslate; ///译文古文
+  final String likeClassic;     ///象文古文
+  final String likeTranslate;   ///象文译文
 }
