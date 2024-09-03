aff611094dd100135103e2fb12185f7ad2009cba
yangfu132 <yangfu132@163.com>
2024-09-03 11:10:21 +0800

    feature：去掉debug标记

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class MyApp extends StatelessWidget {
         //其它Locales
       ],
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
